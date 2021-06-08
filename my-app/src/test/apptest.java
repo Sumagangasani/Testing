d440230c5358fac1751ec94cb666d9608b83af04
EC2 Default User <ec2-user@ip-172-31-14-193.us-east-2.compute.internal>
2021-06-08 15:10:02 +0000

    checking framework

---
+++ my-app/src/test/apptest.java
@@ -0,0 +1,24 @@
+* @param testName name of the test case
+     */
+    public AppTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( AppTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
+
